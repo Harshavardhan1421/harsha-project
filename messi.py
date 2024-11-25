73417502d5c070e23b202012a9dfe7aa9d065cc4
HArshavardhan <Hash@123.com>
2024-11-25 18:28:02 +0530

    added messi.py file

---
+++ messi.py
@@ -0,0 +1,5 @@
+def fun():
+    print("messi played for barcelona fc")
+    print("messi was born in argentina")
+    print("messi is the most celebrated football player in the history of football")
+fun()
